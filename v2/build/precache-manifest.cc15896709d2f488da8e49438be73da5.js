dbdabd1a49050cc36cc58b93b3b7c3255d47bc70
Onenrico PC <enrico.cybernet@gmail.com>
2020-09-09 14:14:24 +0700

    Build

---
+++ v2/build/precache-manifest.cc15896709d2f488da8e49438be73da5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e60d63a0eebbd31a62fda8edeb8648",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86555c2cefaab1431bca",
+    "url": "/static/css/main.b071ffb6.chunk.css"
+  },
+  {
+    "revision": "81fc68302934b6889f3a",
+    "url": "/static/js/2.0aea3eb2.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/static/js/2.0aea3eb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86555c2cefaab1431bca",
+    "url": "/static/js/main.2188c6f5.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  }
+]);
